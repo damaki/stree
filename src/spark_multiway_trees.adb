4a219db5eae436d0f28891240ecf9a220a16a5cc
Daniel King <damaki.gh@gmail.com>
2025-03-29 16:37:53 +0000

    Initial declarations with formal model definition

--- src/spark_multiway_trees.adb
+++ src/spark_multiway_trees.adb
@@ -3,9 +3,375 @@
 --
 --  SPDX-License-Identifier: Apache-2.0
 --
+with SPARK.Containers;
+with SPARK.Containers.Functional.Sets;
+
 package body SPARK_Multiway_Trees with
   SPARK_Mode => On
 is
+
+   package body Formal_Model is
+
+      package I_Sets is new SPARK.Containers.Functional.Sets (Index_Type, "=");
+
+      function All_Indexes return I_Sets.Set with
+        Global => null,
+        Post   => (for all I in Index_Type =>
+                     I_Sets.Contains (All_Indexes'Result, I))
+                  and then I_Sets.Length (All_Indexes'Result)
+                           = To_Big_Integer (Max_Size);
+
+      -------------------------
+      -- Element_Logic_Equal --
+      -------------------------
+
+      function Element_Logic_Equal (Left, Right : Element_Type) return Boolean
+      with SPARK_Mode => Off
+      is
+      begin
+         SPARK.Containers.Check_Or_Fail;
+         return Left = Right;
+      end Element_Logic_Equal;
+
+      -----------------
+      -- Has_Element --
+      -----------------
+
+      function Has_Element (F : Forest; C : Cursor) return Boolean is
+        (C.Node /= Empty and then Contains (F.Nodes, C.Node));
+
+      -------------
+      -- Element --
+      -------------
+
+      function Element (F : Forest; C : Cursor) return Element_Type is
+        (Element (F.Nodes, C.Node).Element);
+
+      ------------
+      -- Parent --
+      ------------
+
+      function Parent (F : Forest; C : Cursor) return Cursor is
+        (Cursor'(Node => Element (F.Nodes, C.Node).Parent));
+
+      -----------
+      -- Child --
+      -----------
+
+      function Child
+        (F : Forest;
+         C : Cursor;
+         W : Way_Type)
+         return Cursor
+      is
+        (Cursor'(Node => Element (F.Nodes, C.Node).Ways (W)));
+
+      -------------
+      -- Is_Root --
+      -------------
+
+      function Is_Root (F : Forest; C : Cursor) return Boolean is
+        (Has_Element (F, C) and then Element (F.Nodes, C.Node).Position = Top);
+
+      --------------
+      -- Position --
+      --------------
+
+      function Position (F : Forest; I : Index_Type) return Way_Type is
+        (Element (F.Nodes, I).Position);
+
+      -----------
+      -- Model --
+      -----------
+
+      function Model (F : Forest; Root : Cursor) return Model_Type is
+         use I_Sets;
+
+         type Boolean_Array is array (Index_Type) of Boolean;
+
+         function Next (Todo : Boolean_Array) return Extended_Index_Type with
+           Post => (if Next'Result = Empty
+                    then (for all T of Todo => not T)
+                    else Todo (Next'Result));
+
+         function Next (Todo : Boolean_Array) return Extended_Index_Type is
+         begin
+            for I in Todo'Range loop
+               pragma Loop_Invariant (for all J in 1 .. I - 1 => not Todo (J));
+               if Todo (I) then
+                  return I;
+               end if;
+            end loop;
+
+            return Empty;
+         end Next;
+
+         Todo : Boolean_Array := [others => False];
+
+         Unseen : I_Sets.Set := All_Indexes;
+
+         M : Model_Type;
+         I : Extended_Index_Type := Root.Node;
+
+      begin
+         --  Insert the root node in the todo list
+
+         Todo (Root.Node)      := True;
+         M (Root.Node).In_Tree := True;
+
+         while I /= Empty loop
+            pragma Loop_Variant (Decreases => Length (Unseen));
+
+            --  Node I is in the todo list and in the tree
+            pragma Loop_Invariant (Todo (I) and then M (I).In_Tree);
+
+            --  All nodes in the todo list are in the tree
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if Todo (J) then M (J).In_Tree));
+
+            --  All nodes in the tree are in the forest
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree then Contains (F.Nodes, J)));
+
+            --  All nodes in the todo list are in the forest
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if Todo (J) then Contains (F.Nodes, J)));
+
+            --  The path of a node in the todo list is maximal w.r.t. other
+            --  nodes which are known to be in the tree at this stage.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if Todo (J) then
+                    (for all K in Index_Type =>
+                       (if M (K).In_Tree
+                        then not (M (J).Path < M (K).Path)))));
+
+            --  Children of nodes in the todo list are not yet in the tree
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if Todo (J) then
+                    (for all W of Element (F.Nodes, J).Ways =>
+                       (if W /= Empty then
+                          not M (W).In_Tree and then not Todo (W)))));
+
+            --  The root is in tree with an empty path
+            pragma Loop_Invariant
+              (M (Root.Node).In_Tree and then Length (M (Root.Node).Path) = 0);
+
+            --  Non-root nodes in the tree don't have position Top and have a
+            --  parent.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree and then J /= Root.Node
+                  then Element (F.Nodes, J).Position /= Top
+                       and then Element (F.Nodes, J).Parent /= Empty));
+
+            --  Non-root nodes are in the tree iff their parent is in the tree
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree and then J /= Root.Node
+                  then Element (F.Nodes, J).Parent /= Empty
+                       and then M (Element (F.Nodes, J).Parent).In_Tree));
+
+            --  The path from the root to non-root tree nodes is equal to the
+            --  path to their parent extended by the last direction to get to
+            --  the node. For all other nodes, the path is empty.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree and then J /= Root.Node
+                  then Is_Add (M (Element (F.Nodes, J).Parent).Path,
+                               Element (F.Nodes, J).Position,
+                               M (J).Path)));
+
+            --  A node known to be in the tree is either the root node, or a
+            --  node whose parent is known to be in the tree. In that case, the
+            --  node is either known to be in the tree, or the parent is still
+            --  in the todo list.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if J /= Root.Node and then Contains (F.Nodes, J) then
+                    (if Element (F.Nodes, J).Parent /= Empty
+                        and then M (Element (F.Nodes, J).Parent).In_Tree
+                     then M (J).In_Tree
+                          or else Todo (Element (F.Nodes, J).Parent)
+                     else not M (J).In_Tree)));
+
+            --  A node known to be in the tree does not have its parent in the
+            --  Todo list.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree and then J /= Root.Node
+                  then not Todo (Element (F.Nodes, J).Parent)));
+
+            --  Nodes in the tree all have different associated paths
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if M (J).In_Tree then
+                    (for all K in Index_Type =>
+                       (if M (K).In_Tree and then M (K).Path = M (J).Path
+                        then J = K))));
+
+            --  The longest path stored so far is of size
+            --  Max_Size - Length (Unseen) at most.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (Length (M (J).Path)
+                  <= To_Big_Integer (Max_Size) - Length (Unseen)));
+
+            --  Nodes that have not been handled yet are either not in the tree
+            --  or in the todo list.
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (Contains (Unseen, J)
+                  = (not M (J).In_Tree or else Todo (J))));
+
+            --  Every node not in the tree has no path
+            pragma Loop_Invariant
+              (for all J in Index_Type =>
+                 (if not M (J).In_Tree then Length (M (J).Path) = 0));
+
+            Unseen := Remove (Unseen, I);
+
+            declare
+               J : Extended_Index_Type;
+            begin
+               J := Element (F.Nodes, I).Ways (1);
+               if J /= Empty then
+                  pragma Assert
+                    (Element (F.Nodes, Element (F.Nodes, I).Ways (1)).Parent
+                     = I);
+                  pragma Assert (not Is_Empty (Unseen));
+
+                  Todo (J) := True;
+                  M (J)    := (In_Tree => True,
+                               Path    => Add (M (I).Path, 1));
+               end if;
+            end;
+
+            declare
+               J : Extended_Index_Type;
+            begin
+               J := Element (F.Nodes, I).Ways (2);
+               if J /= Empty then
+                  pragma Assert (not Is_Empty (Unseen));
+
+                  Todo (J) := True;
+                  M (J)    := (In_Tree => True,
+                               Path    => Add (M (I).Path, 2));
+               end if;
+            end;
+
+            --  Nothing more to do for node I
+
+            Todo (I) := False;
+            I        := Next (Todo);
+         end loop;
+
+         return M;
+      end Model;
+
+      -----------------
+      -- All_Indexes --
+      -----------------
+
+      function All_Indexes return I_Sets.Set is
+         use I_Sets;
+         S : I_Sets.Set;
+      begin
+         for I in Index_Type loop
+            pragma Loop_Invariant (for all J in 1 .. I - 1 => Contains (S, J));
+            pragma Loop_Invariant (for all J of S => J < I);
+            pragma Loop_Invariant
+              (Length (S) = To_Big_Integer (Integer (I - Index_Type'First)));
+
+            S := Add (S, I);
+         end loop;
+
+         return S;
+      end All_Indexes;
+
+   end Formal_Model;
+
+   -----------------
+   -- Has_Element --
+   -----------------
+
+   function Has_Element
+     (Container : Forest;
+      Position  : Cursor) return Boolean
+   is
+     (Position.Node /= Empty
+      and then Contains (Container.Nodes, Position.Node));
+
+   -------------
+   -- Element --
+   -------------
+
+   function Element
+     (Container : Forest;
+      Position  : Cursor)
+      return Element_Type
+   is
+      E_Acc : constant access constant Node_Type :=
+                  Constant_Reference (Container.Nodes, Position.Node);
+   begin
+      return E_Acc.all.Element;
+   end Element;
+
+   -------------
+   -- Is_Root --
+   -------------
+
+   function Is_Root
+     (Container : Forest;
+      Position  : Cursor)
+      return Boolean
+   is
+      E_Acc : constant access constant Node_Type :=
+                Constant_Reference (Container.Nodes, Position.Node);
+   begin
+      return E_Acc.all.Position = Top;
+   end Is_Root;
+
+   ------------
+   -- Parent --
+   ------------
+
+   function Parent
+     (Container : Forest;
+      Position  : Cursor)
+      return Cursor
+   is
+      E_Acc : constant access constant Node_Type :=
+                Constant_Reference (Container.Nodes, Position.Node);
+   begin
+      return Cursor'(Node => E_Acc.all.Parent);
+   end Parent;
+
+   -----------
+   -- Child --
+   -----------
+
+   function Child
+     (Container : Forest;
+      Position  : Cursor;
+      Way       : Way_Type)
+      return Cursor
+   is
+      E_Acc : constant access constant Node_Type :=
+                Constant_Reference (Container.Nodes, Position.Node);
+   begin
+      return Cursor'(Node => E_Acc.all.Ways (Way));
+   end Child;
+
+   function Constant_Reference
+     (Container : Forest;
+      Position  : Cursor) return not null access constant Element_Type
+   is
+     (Constant_Reference (Container.Nodes, Position.Node).all.Element'Access);
 
    --------------------
    -- Tree_Structure --
@@ -15,32 +381,44 @@ is
      (
       --  The parent of a node is either Empty or references another, valid
       --  node.
-      (for all I of F =>
-         Element (F, I).Parent = Empty
-         or else Contains (F, Element (F, I).Parent))
+      (for all I in Index_Type =>
+         (if Contains (F, I) and then Element (F, I).Parent /= Empty
+          then Contains (F, Element (F, I).Parent)))
 
       --  Each way of a node is either Empty or references another, valid node.
       and then
-        (for all I of F =>
-           (for all W of Element (F, I).Ways =>
-              W = Empty or else Contains (F, W)))
+        (for all I in Index_Type =>
+           (if Contains (F, I) then
+              (for all W of Element (F, I).Ways =>
+                 (if W /= Empty then Contains (F, W)))))
 
       --  If a node has position Top then it has no parent, otherwise it
       --  has a valid parent
       and then
-        (for all I of F =>
-           (if Element (F, I).Position = Top
-            then Element (F, I).Parent = Empty
-            else Element (F, I).Parent /= Empty
-                 and then Contains (F, Element (F, I).Parent)))
+        (for all I in Index_Type =>
+           (if Contains (F, I) then
+              (if Element (F, I).Position = Top
+               then Element (F, I).Parent = Empty
+               else Element (F, I).Parent /= Empty
+                    and then Contains (F, Element (F, I).Parent))))
 
       --  If a node is a child (has a position), then it is the child of its
       --  parent.
       and then
-        (for all I of F =>
-           (if Element (F, I).Position /= Top then
+        (for all I in Index_Type =>
+           (if Contains (F, I) and then Element (F, I).Position /= Top then
               Element (F, Element (F, I).Parent).Ways (Element (F, I).Position)
                 = I))
+
+      --  Every child of node I has I as its parent
+      and then
+        (for all I in Index_Type =>
+           (if Contains (F, I) then
+              (for all W in Way_Type =>
+                 (if Element (F, I).Ways (W) /= Empty then
+                    Element (F, Element (F, I).Ways (W)).Position = W
+                    and then Element (F, Element (F, I).Ways (W)).Parent
+                             = I))))
      );
 
 end SPARK_Multiway_Trees;
--- src/spark_multiway_trees.ads
+++ src/spark_multiway_trees.ads
@@ -3,26 +3,258 @@
 --
 --  SPDX-License-Identifier: Apache-2.0
 --
+with Ada.Containers;
+
+with SPARK.Big_Integers; use SPARK.Big_Integers;
+with SPARK.Containers.Functional.Vectors;
+
 private with SPARK.Containers.Formal.Unbounded_Ordered_Maps;
 
 package SPARK_Multiway_Trees with
   SPARK_Mode => On
 is
 
+   use type Ada.Containers.Count_Type;
+
    type Element_Type is new Integer;
-   type Index_Type is new Positive;
-   Order : constant Positive := 5;
+   Order : constant Positive := 2;
 
    type Way_Type is new Positive range 1 .. Order;
 
    type Forest is private;
 
-private
+   type Index_Type is new Ada.Containers.Count_Type
+     range 1 .. Ada.Containers.Count_Type'Last - 1;
 
    subtype Extended_Index_Type is Index_Type'Base
      range Index_Type'First - 1 .. Index_Type'Last;
 
-   Empty : constant := Index_Type'First - 1;
+   type Cursor is record
+      Node : Extended_Index_Type := Extended_Index_Type'First;
+   end record;
+
+   No_Element : constant Cursor;
+
+   ------------------
+   -- Formal Model --
+   ------------------
+
+   package Formal_Model with Ghost is
+
+      function Element_Logic_Equal (Left, Right : Element_Type) return Boolean
+      with
+        Global => null,
+        Annotate => (GNATprove, Logical_Equal);
+
+      -----------
+      -- Paths --
+      -----------
+
+      Max_Size : constant := (Index_Type'Last - Index_Type'First) + 1;
+
+      type Positive_Count_Type is range 1 .. Max_Size + 1;
+      package Way_Seqs is new SPARK.Containers.Functional.Vectors
+        (Positive_Count_Type, Way_Type);
+      use Way_Seqs;
+
+      type Path_Type is record
+         Path    : Way_Seqs.Sequence;
+         In_Tree : Boolean := False;
+      end record;
+
+      function Is_Concat (Q, V, P : Sequence) return Boolean is
+        (Length (P) - Length (V) = Length (Q)
+         and then (for all I in 1 .. Last (Q) => Get (P, I) = Get (Q, I))
+         and then (for all I in 1 .. Last (V) =>
+                    Get (P, I + Last (Q)) = Get (V, I))
+         and then (for all I in Last (Q) + 1 .. Last (P) =>
+                    Get (V, I - Last (Q)) = Get (P, I)))
+      with Pre => Length (Q) <= Big_Integer'(Max_Size);
+      --  Returns True if P is the concatenation of Q & V.
+
+      function Is_Add
+        (S1 : Sequence;
+         W  : Way_Type;
+         S2 : Sequence)
+         return Boolean
+      is
+        (Length (S2) - 1 = Length (S1) and then S1 < S2
+         and then Get (S2, Last (S2)) = W);
+      --  Returns True if S2 is equal to S1 with W appended to it
+
+      ---------------
+      -- Accessors --
+      ---------------
+
+      function Has_Element (F : Forest; C : Cursor) return Boolean with
+        Global => null,
+        Post => (if C = No_Element then not Has_Element'Result);
+
+      function Has_Element (F : Forest; I : Index_Type) return Boolean is
+        (Has_Element (F, Cursor'(Node => I)))
+      with
+        Global => null;
+
+      function Element (F : Forest; C : Cursor) return Element_Type with
+        Global => null,
+        Pre => Has_Element (F, C);
+
+      function Element (F : Forest; I : Index_Type) return Element_Type is
+        (Element (F, Cursor'(Node => I)))
+      with
+        Global => null,
+        Pre => Has_Element (F, I);
+
+      function Parent (F : Forest; C : Cursor) return Cursor with
+        Global => null,
+        Pre    => Has_Element (F, C),
+        Post   => (if Parent'Result /= No_Element
+                   then Has_Element (F, Parent'Result));
+
+      function Parent (F : Forest; I : Index_Type) return Cursor is
+        (Parent (F, Cursor'(Node => I)))
+      with
+        Global => null,
+        Pre    => Has_Element (F, I);
+
+      function Child
+        (F : Forest;
+         C : Cursor;
+         W : Way_Type)
+         return Cursor
+      with
+        Global => null,
+        Pre    => Has_Element (F, C),
+        Post   => (if Child'Result /= No_Element
+                   then Has_Element (F, Child'Result));
+
+      function Is_Root (F : Forest; C : Cursor) return Boolean with
+        Global => null,
+        Pre    => Has_Element (F, C),
+        Post   => (if Is_Root'Result then Parent (F, C) = No_Element);
+
+      function Position (F : Forest; I : Index_Type) return Way_Type with
+        Ghost,
+        Global => null,
+        Pre    => Has_Element (F, I)
+                  and then Parent (F, I) /= No_Element;
+
+      -----------
+      -- Model --
+      -----------
+
+      type Model_Type is array (Index_Type) of Path_Type with
+        Predicate =>
+          (for all P of Model_Type =>
+             Way_Seqs.Length (P.Path) < Big_Integer'(Max_Size));
+
+      function Model (F : Forest; Root : Cursor) return Model_Type with
+        Global => null,
+        Pre    => Root /= No_Element
+                  and then Has_Element (F, Root.Node)
+                  and then Is_Root (F, Root),
+        Post   =>
+          --  The root is part of the tree
+          Model'Result (Root.Node).In_Tree
+
+          --  The path from the root to itself is empty
+          and then Last (Model'Result (Root.Node).Path) = 0
+
+          --  Non-root nodes are in the tree iff their parent is in the tree
+          and then
+            (for all I in Index_Type =>
+               (if I /= Root.Node and then Has_Element (F, I) then
+                  (if Parent (F, I) /= No_Element
+                      and then Model'Result (Parent (F, I).Node).In_Tree
+                   then Model'Result (I).In_Tree
+                   else not Model'Result (I).In_Tree)))
+
+          --  The path from the root to non-root tree nodes is equal to the
+          --  path to their parent extended by the last direction to get to the
+          --  node. For other nodes, the path is empty.
+          and then
+            (for all I in Index_Type =>
+               (if Model'Result (I).In_Tree and then I /= Root.Node
+                then Is_Add (Model'Result (Parent (F, I).Node).Path,
+                             Position (F, I),
+                             Model'Result (I).Path)
+                else Last (Model'Result (I).Path) = 0))
+
+          --  Nodes in the tree all have different associated paths
+          and then
+            (for all I in Index_Type =>
+               (if Model'Result (I).In_Tree then
+                  (for all J in Index_Type =>
+                     (if Model'Result (J).In_Tree
+                         and then Model'Result (J).Path = Model'Result (I).Path
+                      then J = I))))
+
+          --  All nodes in the tree map to a valid element
+          and then
+            (for all I in Index_Type =>
+               (if Model'Result (I).In_Tree then Has_Element (F, I)));
+
+   end Formal_Model;
+
+   function Has_Element
+     (Container : Forest;
+      Position  : Cursor)
+      return Boolean
+   with
+     Global => null,
+     Post =>
+       Has_Element'Result = Formal_Model.Has_Element (Container, Position);
+
+   function Element
+     (Container : Forest;
+      Position  : Cursor)
+      return Element_Type
+   with
+     Global => null,
+     Pre => Has_Element (Container, Position),
+     Post => Element'Result = Formal_Model.Element (Container, Position);
+
+   function Is_Root
+     (Container : Forest;
+      Position  : Cursor)
+      return Boolean
+   with
+     Global => null,
+     Pre  => Has_Element (Container, Position),
+     Post => Is_Root'Result = Formal_Model.Is_Root (Container, Position);
+
+   function Parent
+     (Container : Forest;
+      Position  : Cursor)
+      return Cursor
+   with
+     Global => null,
+     Pre => Has_Element (Container, Position),
+     Post => Parent'Result = Formal_Model.Parent (Container, Position);
+
+   function Child
+     (Container : Forest;
+      Position  : Cursor;
+      Way       : Way_Type)
+      return Cursor
+   with
+     Global => null,
+     Pre => Has_Element (Container, Position),
+     Post => Child'Result = Formal_Model.Child (Container, Position, Way);
+
+   function Constant_Reference
+     (Container : Forest;
+      Position  : Cursor) return not null access constant Element_Type
+   with
+     Global => null,
+     Pre    => Has_Element (Container, Position),
+     Post   => Formal_Model.Element_Logic_Equal
+                 (Constant_Reference'Result.all,
+                  Formal_Model.Element (Container, Position));
+
+private
+
+   Empty : constant := Extended_Index_Type'First;
 
    type Way_Array is array (Way_Type) of Extended_Index_Type;
 
@@ -31,8 +263,10 @@ private
 
    Top : constant Position_Type := Position_Type'First;
 
+   No_Element : constant Cursor := Cursor'(Node => Empty);
+
    type Node_Type is record
-      Element  : Element_Type;
+      Element  : aliased Element_Type;
       Parent   : Extended_Index_Type;
       Position : Position_Type;
       Ways     : Way_Array;
@@ -46,8 +280,9 @@ private
    use Node_Maps;
 
    type Forest is record
-      Nodes : Node_Maps.Map;
-   end record;
+      Nodes : aliased Node_Maps.Map;
+   end record with
+     Type_Invariant => Tree_Structure (Nodes);
 
    function Tree_Structure (F : Node_Maps.Map) return Boolean with
      Ghost,
